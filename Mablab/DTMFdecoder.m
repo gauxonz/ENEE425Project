8ba80050caa29acadaae67a37c3bbdf555375d7c
gauxonz <gongzheng.joey@gmail.com>
2013-05-01 17:14:33 -0400

    envelope funstion added

--- Mablab/DTMFdecoder.m
+++ Mablab/DTMFdecoder.m
@@ -1,20 +1,22 @@
 function result = DTMFdecoder(WaveFile,option)
-
-SampleLength = 1024;
-
-[RawWave,Fs,Nbits,Info]=wavread(WaveFile,[1001 SampleLength+1001-1]);
+%read file
+[RawWave,Fs,Nbits,Info]=wavread(WaveFile);
 %draw raw wave
 subplot(2,1,1)
 plot(RawWave);
 title('DTMF Raw Input');xlabel('Time');
 ylabel('Amplitude');grid;
-%do fft
+%do fft and draw
 Ts=1/Fs;
 FFTWave=abs(fft(RawWave,length(RawWave)));
 subplot(2,1,2)
-plot(Fs/SampleLength.*[1:SampleLength/2],FFTWave(1:SampleLength/2));
+plot(Fs/length(FFTWave).*[1:length(FFTWave)/2],FFTWave(1:length(FFTWave)/2));
 title('FFT');xlabel('Continue Freq');
 ylabel('Amplitude');grid;
 %decode single 
-result=DTMFdecoder_single(RawWave,Fs);
+
+%%temp
+SampleLength = 1024;
+result=DTMFdecoder_single(RawWave(1001:SampleLength+1001-1),Fs);
+
 disp( ['The code is ' result]) 
